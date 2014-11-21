47fc8a6c711af8eecaaad059ce9842750951ab0f
sunsetgit <sunset_xinl@sina.com>
2014-11-21 11:39:39 +0800

    2
    2

---
+++ android/src/com/example/android/TextViewForImg.java
@@ -0,0 +1,87 @@
+package com.example.android;
+
+import java.lang.reflect.Field;
+
+import android.R.color;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Html.ImageGetter;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+/**
+ * textView 显示img
+ * 
+ * @author Administrator
+ *
+ */
+public class TextViewForImg extends Activity {
+
+	private TextView textViewImg;
+
+	public TextViewForImg() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.text_view);
+
+		textViewImg = (TextView) findViewById(R.id.textImg_1);
+		textViewImg.setTextColor(color.white);
+		textViewImg.setBackgroundColor(color.black);
+		textViewImg.setTextSize(20);
+		// 设定html标签样式,图片3为一个超链接标签
+		String html = "图像1<img src='image1'/>图像2<img src='image2'/>\n";
+		html += "图像3<a href='http://www.baidu.com'><img src='image3'/></a>";
+		// fromhtml中imageGetter选择html中<img>图片资源
+		CharSequence charSequence = Html.fromHtml(html, new ImageGetter() {
+
+			@Override
+			public Drawable getDrawable(String source) {
+				// TODO Auto-generated method stub
+				// source为html字符串中的定义的<img>的src的内容
+				// 返回值Frawable就是对应的<img>显示的图片资源
+				Drawable draw = null;
+				if (source.equals("image1")) {
+					draw = getResources().getDrawable(R.drawable.image1);
+					draw.setBounds(0, 0, draw.getIntrinsicWidth(),
+							draw.getIntrinsicHeight());
+				} else if (source.equals("image2")) {
+					// 设置image2尺寸等比缩小
+					draw = getResources().getDrawable(R.drawable.image2);
+					draw.setBounds(0, 0, draw.getIntrinsicWidth(),
+							draw.getIntrinsicHeight());
+				} else {
+					// 使用反射会更简便,无需知道src与资源Id的对应关系
+					draw = getResources().getDrawable(getResourceId(source));
+					draw.setBounds(0, 0, draw.getIntrinsicWidth(),
+							draw.getIntrinsicHeight());
+				}
+				return draw;
+			}
+		}, null);
+		textViewImg.setText(charSequence);
+		textViewImg.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+
+	public int getResourceId(String source) {
+		// 使用反射机制,通过属性名称,得到其内的值
+		Field field;
+		try {
+			field = R.drawable.class.getField(source);
+			return Integer.parseInt(field.get(null).toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return 0;
+	}
+
+}
---
+++ android/src/com/example/android/TextViewOnClick.java
@@ -0,0 +1,75 @@
+package com.example.android;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * 点击activity间跳转
+ * 
+ * @author Administrator
+ *
+ */
+public class TextViewOnClick extends Activity {
+
+	private TextView clickTextView1, clickTextView2, clickTextView3;
+
+	public TextViewOnClick() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.text_view);
+
+		clickTextView1 = (TextView) findViewById(R.id.textView_1);
+		clickTextView2 = (TextView) findViewById(R.id.textView_2);
+		clickTextView3 = (TextView) findViewById(R.id.textView_3);
+
+		String text1 = "显示activity1";
+		String text2 = "显示activity2";
+		// 使用SpannableString存放字符串
+		SpannableString spannableString = new SpannableString(text1);
+		SpannableString spannableString2 = new SpannableString(text2);
+		// 通过setSpan设定文本块相应的点击事件
+		// 此处只设定文本的索引为2开始的文本块:acitivity1
+		spannableString.setSpan(new ClickableSpan() {
+
+			@Override
+			public void onClick(View widget) {
+				// TODO Auto-generated method stub
+				// 导航到一个新的activity中
+				Intent intent = new Intent(TextViewOnClick.this,
+						MainActivity.class);
+				startActivity(intent);
+			}
+		}, 2, text1.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+		spannableString2.setSpan(new ClickableSpan() {
+
+			@Override
+			public void onClick(View widget) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(TextViewOnClick.this,
+						TextViewDemo.class);
+				startActivity(intent);
+			}
+		}, 2, text1.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+		// 对textView文本进行赋值
+		clickTextView1.setText(spannableString);
+		// 设置点击相应
+		clickTextView1.setMovementMethod(LinkMovementMethod.getInstance());
+		clickTextView2.setText(spannableString2);
+		clickTextView2.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+
+}
